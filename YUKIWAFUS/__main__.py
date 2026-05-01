918a465ec6cca33355182e68f8b7a417ff65f0d2
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-05-01 13:41:42 +0530

    Update __main__.py

--- YUKIWAFUS/__main__.py
+++ YUKIWAFUS/__main__.py
@@ -1,1 +1,33 @@
+import asyncio
+import importlib
 
+from pyrogram import idle
+
+import config
+from YUKIWAFUS import LOGGER, app
+from YUKIWAFUS.modules import ALL_MODULES
+
+
+async def init():
+    await app.start()
+
+    for module in ALL_MODULES:
+        importlib.import_module("YUKIWAFUS.modules." + module)
+
+    LOGGER.info("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    LOGGER.info("  ✦ All Modules Loaded  ✦")
+    LOGGER.info("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+    LOGGER.info(
+        "╔═════ஜ۩۞۩ஜ════╗\n"
+        "  ✦ YUKIWAFUS Started ✦\n"
+        "╚═════ஜ۩۞۩ஜ════╝"
+    )
+
+    await idle()
+    await app.stop()
+    LOGGER.info("YUKIWAFUS Stopped.")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
+  
